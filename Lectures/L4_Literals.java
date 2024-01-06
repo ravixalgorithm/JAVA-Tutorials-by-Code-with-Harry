159cc7c3b017834e8f86301bbf9a71afebb4d859
Mr. Algorithm <148683640+ravixalgorithm@users.noreply.github.com>
2024-01-06 23:24:33 +0530

    Shifting to GitHub

---
+++ Lectures/L4_Literals.java
@@ -0,0 +1,18 @@
+
+
+public class L4_Literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        int age2 = 56;
+        short age3 = 94;
+        long ageDino = 5666663242344L;
+        char ch = 'R';
+        float f1 = 4.56f;
+        double d1 = 332232.232322;
+        boolean bool = true;
+        System.out.println(bool);
+
+        String str = "RAVI";
+        System.out.println(str);
+    }
+}
