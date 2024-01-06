6b77f5b06d0f9939dab78e13cf8555b0aa07aa10
Mr. Algorithm <148683640+ravixalgorithm@users.noreply.github.com>
2024-01-06 23:25:34 +0530

    Shifting to GitHub

---
+++ Lectures/L5_TakingInput.java
@@ -0,0 +1,24 @@
+
+
+import java.util.Scanner;
+
+public class L5_TakingInput {
+    public static void main(String[] args) {
+        System.out.println("Taking input from the User");
+        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter number 1 : ");
+////        int a = sc.nextInt();
+//        float a = sc.nextFloat();
+//        System.out.print("Enter number 2 : ");
+////        int b = sc.nextInt();
+//        float b = sc.nextFloat();
+//        float sum = a+b;
+//        System.out.println("Sum of these numbers is "+sum);
+//        boolean b1 = sc.hasNextInt();
+//        System.out.println(b1);
+//        String str = sc.next();
+        String str = sc.nextLine();
+        System.out.println(str);
+
+    }
+}
---
+++ Lectures/L8_Operators.java
@@ -0,0 +1,30 @@
+
+
+public class L8_Operators {
+    public static void main(String[] args) {
+        // 1. Arithmetic Operators
+        int a = 4;
+//        int b = 9%a; //Modulo Operator
+//        4.8%1.1 --> Returns Decimal Remainder
+
+        // 2. Assignment Operators
+        int b = 9;
+        b *= 3;
+        System.out.println(b);
+
+        // 3. Comparison Operator
+//        System.out.println(565<65);
+
+        // 4. Logical Operators
+        System.out.println(64>5 && 64>98); //AND Operator
+        System.out.println(64>5 || 64>98); //OR Operator
+
+        // 5. Bitwise Operators
+        System.out.println(2&3);
+
+        // 10
+        // 11
+        //-----
+        // 10
+    }
+}
