5ecc1b9c2a83c8baf6db51a5cdcf452387868897
Mr. Algorithm <148683640+ravixalgorithm@users.noreply.github.com>
2024-01-06 23:35:55 +0530

    Create L16_Conditionals.java

---
+++ Lectures/L16_Conditionals.java
@@ -0,0 +1,14 @@
+
+
+public class L16_Conditionals {
+    public static void main(String[] args) {
+        int age = 8 ;
+        boolean cond = age > 18;
+        if(cond){
+            System.out.println("Yes you can Drive");
+        }
+        else{
+            System.out.println("No you can't Drive");
+        }
+    }
+}
