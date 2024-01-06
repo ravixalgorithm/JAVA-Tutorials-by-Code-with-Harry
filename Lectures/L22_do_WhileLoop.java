c680bd4eb2d095e63b5da463bca3c39c1ebd32fb
Mr. Algorithm <148683640+ravixalgorithm@users.noreply.github.com>
2024-01-06 23:38:09 +0530

    L22_do_WhileLoop

---
+++ Lectures/L22_do_WhileLoop.java
@@ -0,0 +1,18 @@
+public class L22_do_WhileLoop {
+    public static void main(String[] args) {
+        // While LOOP --> (first Check then proceed)
+        // int i = 10;
+        // while(i<5){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+
+        // Do-While LOOP --> (First run then check, thus it run atleast Once)
+        int j = 20;
+        do{
+            System.out.println(j);
+            j++;
+        }while(j<5);
+    }
+}
