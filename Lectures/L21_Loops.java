871526051e507ba6d1340f391179b0f0771c86c4
Mr. Algorithm <148683640+ravixalgorithm@users.noreply.github.com>
2024-01-06 23:37:17 +0530

    create L21_loops

---
+++ Lectures/L21_Loops.java
@@ -0,0 +1,22 @@
+public class L21_Loops{
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+
+        System.out.println("Using While Loop: ");
+        // While Loop -->
+        int i = 1;
+        while(i<=3){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Finish Running While Loop! ");
+
+        // while(true){
+        //     System.out.println("I am an Infinite While Loop");
+        // }
+    
+    }
+
+}
