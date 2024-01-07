24d46c24f2d6995b8f2f577f108fa5e98204277a
Mr. Algorithm <148683640+ravixalgorithm@users.noreply.github.com>
2024-01-07 14:49:21 +0530

    Create L24_Break_and_Continue

---
+++ Lectures/L24_Break_and_Continue.java
@@ -0,0 +1,58 @@
+public class L24_Break_and_Continue {
+    public static void main(String[] args) {
+        // Break and continue using loops!
+
+        
+        // Break -->
+
+        // Case 1 -> For Loop
+        // for(int i = 0; i<50; i++){
+        //     System.out.println(i);
+        //     System.out.println("Java is the best");
+        //     if(i==4){
+        //         System.out.println("Ending the Loop!!!");
+        //         break;
+        //     }
+        // }
+        // System.out.println("Loop Ends Here!!!");
+
+        // Case 2 -> Do-While Loop
+        // int i = 0;
+        // do{
+        //     System.out.println(i);
+        //     System.out.println("Java is the Best");
+        //     if(i==3){
+        //         System.out.println("Ending the loop");
+        //         break;
+        //     }
+        //     i++;
+        // }while(i<20);
+        // System.out.println("Loop ends here");
+
+
+        // Continue -->
+
+        // Case 1 -> For Loop
+        // for(int i = 0; i<25; i++){
+        //     if(i==2){
+        //         System.out.println("skipping the "+ i + ", Moving to the next");
+        //         continue;
+        //     }
+        //     System.out.println(i);
+        //     System.out.println("Java is the best");
+        // }
+
+        // Case 2 -> Do-While Loop
+        // int i=0;
+        // do{
+        //     i++;
+        //     if(i==5){
+        //         System.out.println("skipping the "+ i + ", Moving to the next");
+        //         continue;
+        //     }
+        //     System.out.println(i);
+        //     System.out.println("Java is the Best");
+        // }while(i<25);
+
+    }
+}
