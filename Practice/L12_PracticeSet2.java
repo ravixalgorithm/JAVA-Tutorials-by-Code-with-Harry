fc36b7bf1b3afc7d13b4c8ec2bbd93479d8fedff
Mr. Algorithm <148683640+ravixalgorithm@users.noreply.github.com>
2024-01-10 16:48:40 +0530

    Create L12_PracticeSet2.java

---
+++ Practice/L12_PracticeSet2.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class L12_PracticeSet2 {
+    public static void main(String[] args) {
+
+        // Question 1
+        float a = 7/4*9/2;
+        /*
+           = 1*9/2 (7/4 = 1.75 taken as 1)
+           = 9/2
+           = 4 (9/2 = 4.5 taken as 4)
+        */
+//        System.out.println(a);
+
+        // Question 4
+        /*
+           = (v*v - u*u)/2*a*s
+        */
+
+        // Question 5
+        int A = 7*49/7 + 35/7;
+        /*
+           = 343/7 + 35/7
+           = 49 + 5 (precedence of /,* > +,-)
+           = 54
+        */
+//        System.out.println(A);
+
+
+
+
+    }
+}
