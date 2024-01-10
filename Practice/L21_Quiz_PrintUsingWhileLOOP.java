c3ad472361ab3291f83ba7846bdab7bf5a14fc5a
Mr. Algorithm <148683640+ravixalgorithm@users.noreply.github.com>
2024-01-10 16:50:04 +0530

    Create L21_Quiz

---
+++ Practice/L21_Quiz_PrintUsingWhileLOOP.java
@@ -0,0 +1,9 @@
+public class L21_Quiz_PrintUsingWhileLOOP{
+    public static void main(String[] args) {
+        int i = 100;
+        while(i<=200){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
