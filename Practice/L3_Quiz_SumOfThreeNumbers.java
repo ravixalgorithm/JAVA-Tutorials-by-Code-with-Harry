9116bf852e17acf5813ddd802cc9de224a9303e1
Mr. Algorithm <148683640+ravixalgorithm@users.noreply.github.com>
2024-01-10 16:47:28 +0530

    Create L3_Quiz_SumOfThreeNumbers.java

---
+++ Practice/L3_Quiz_SumOfThreeNumbers.java
@@ -0,0 +1,9 @@
+public class L3_Quiz_SumOfThreeNumbers {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 45;
+        int num3 = 332;
+        int sum = num1+num2+num3;
+        System.out.println("The sum of these numbers = "+ sum);
+    }
+}
