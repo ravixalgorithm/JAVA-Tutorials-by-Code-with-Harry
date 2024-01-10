4280c06acc3d10a80cf9df79dd91d6acecff4c12
Mr. Algorithm <148683640+ravixalgorithm@users.noreply.github.com>
2024-01-10 16:49:16 +0530

    Create L19_PracticeSet4.java

---
+++ Practice/L19_PracticeSet4.java
@@ -0,0 +1,152 @@
+import java.util.Scanner;
+
+public class L19_PracticeSet4 {
+    public static void main(String[] args) {
+
+        //Note:- Un-Comment the Code you want to Run
+
+    //     //Question 1 --> Find the Output
+    //    int age = 10;
+    //    if (age = 11){ //Shows an Error, instead == should be used...
+    //        System.out.println("You are 11");
+    //    }
+    //    else{
+    //        System.out.println("You are not 11");
+    //    }
+
+
+        // //Question 2 --> Percentage Calculator with conditions
+        // byte phy, chem, maths;
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Physics : ");
+        // phy = sc.nextByte();
+        // System.out.print("Chemistry : ");
+        // chem = sc.nextByte();
+        // System.out.print("Maths : ");
+        // maths = sc.nextByte();
+
+        // double percent = (phy+maths+chem)/300*100;
+
+        // if(percent>=40 && phy>=33 && chem>=33 && maths>=33){
+        //     System.out.println("Congratulations, You have been Promoted !!");
+        // }
+        // else{
+        //     System.out.println("Sorry, You are failed");
+        // }
+
+
+        // //Question 3 --> Income tax Calculator( OLd Regime )
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter your Income(in lakhs): ");
+
+        // float tax = 0;
+        // float income = sc.nextFloat();
+        // if(income<=2.5f){
+        //     tax = tax + 0;
+        // }
+        // else if(income>2.5f && income<=5.0f){
+        //     tax = tax + 0.05f * (income - 2.5f);
+        // }
+        // else if(income>5.0f && income>=10.0f){
+        //     tax = tax + 0.05f * (5.0f - 2.5f);
+        //     tax = tax + 0.2f * (income - 5.0f);
+        // }
+        // else if(income>10.0f){
+        //     tax = tax + 0.05f * (5.0f - 2.5f);
+        //     tax = tax + 0.2f * (10.0f - 5.0f);
+        //     tax = tax + 0.3f * (income - 10.0f);
+        // }
+        // System.out.println(" ");
+
+        // System.out.println("Total Tax to be paid: " + tax + "Lakhs");
+
+
+        //Question 3 --> New Regime to be done soon
+
+        // //Question 4 --> Program to find Day of the week
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the day of the week: ");
+        // int day = sc.nextInt();
+        // System.out.println(" ");
+
+        // //Traditional Switch Case :-
+
+        // switch(day){
+        //     case 1 :
+        //         System.out.println("Monday");
+        //         break;
+        //     case 2 :
+        //         System.out.println("Tuesday");
+        //         break;
+        //     case 3 :
+        //         System.out.println("Wednesday");
+        //         break;
+        //     case 4 :
+        //         System.out.println("Thursday");
+        //         break;
+        //     case 5 :
+        //         System.out.println("Friday");
+        //         break;
+        //     case 6 :
+        //         System.out.println("Saturday");
+        //         break;
+        //     case 7 :
+        //         System.out.println("Sunday");
+        //         break;
+        //     default:
+        //         System.out.println("Kya phook ke aaya hai bey!! \nTheir are only 7 days in a week");
+        // }
+
+        // //Enhanced Switch Case :-
+        // switch(day){
+        //     case 1 -> System.out.println("Monday");
+        //     case 2 -> System.out.println("Tuesday");
+        //     case 3 -> System.out.println("Wednesday");
+        //     case 4 -> System.out.println("Thursday");
+        //     case 5 -> System.out.println("Friday");
+        //     case 6 -> System.out.println("Saturday");
+        //     case 7 -> System.out.println("Sunday");
+        //     default -> System.out.println("Kya phook ke aaya hai bey!! \nThere are only 7 days in a week");
+        // }
+
+        // //Question 5 --> Check for leap year-
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the Year : ");
+        // int year = sc.nextInt();
+        //  if (year%4 == 0){
+        //     System.out.println("It's a leap year");
+        //  }
+        //  else{
+        //     System.out.println("It's not a leap year");
+        //  }
+
+
+        //  //Question 6 --> Find the type of Website With the URL
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the url of Website : ");
+        // String ws = sc.nextLine();
+        // System.out.println(" ");
+        // if(ws.endsWith(".com")){
+        //     System.out.println("Commercial Website");
+        // }
+        // else if(ws.endsWith(".org")){
+        //     System.out.println("Organization Website");
+        // }
+        // else if(ws.endsWith(".in")){
+        //     System.out.println("Indian Website");
+        // }
+        // else{
+        //     System.out.println("Not Recognized By our System \nWe will surely Update our System Soon");
+        // }
+
+
+
+
+
+
+
+
+
+
+    }
+}
